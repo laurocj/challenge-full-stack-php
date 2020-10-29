8bfbb9b8ac098f39631e679645955b276db4c9e0
laurocj <laurocj@gmail.com>
2020-10-29 19:03:09 -0300

    remover unsed pages

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'Auth\LoginController@showLoginForm');
 
 Route::middleware('auth')
     ->prefix('cms')
