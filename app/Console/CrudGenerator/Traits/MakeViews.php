c33b5469145532e3e2eaeb0ef6d015daa2c9460b
laurocj <laurocj@gmail.com>
2020-10-31 08:41:53 -0300

    type checkbox in CRUDGenerator

--- app/Console/CrudGenerator/Traits/MakeViews.php
+++ app/Console/CrudGenerator/Traits/MakeViews.php
@@ -56,7 +56,9 @@ trait MakeViews
             if ($value->Key != 'PRI' && $value->Field != 'deleted_at' && $value->Field != 'created_at' && $value->Field != 'updated_at') {
 
                 $type = 'text';
-                if (Str::is('*text*', $value->Type)) {
+                if (Str::is('*boolean*', $value->Type) || Str::is('*tinyint*', $value->Type)) {
+                    $type = 'checkbox';
+                } elseif (Str::is('*text*', $value->Type)) {
                     $type = 'textarea';
                 } elseif (Str::is('*int*', $value->Type)) {
                     $type = 'number';
@@ -78,9 +80,9 @@ trait MakeViews
     /**
      * Build the form fields for form.
      *
-     * @param $type
-     * @param $column
      * @param string $type
+     * @param string $column
+     * @param boolean $value
      *
      * @return mixed
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
@@ -88,25 +90,31 @@ trait MakeViews
      */
     protected function getField($type, $column, $value = false)
     {
+        $model = ($type == 'checkbox' || $type == 'radio') ? 'check' : 'input';
+
         $replace = array_merge($this->buildReplacements(), [
             '{{type}}' => $type,
             '{{column}}' => $column,
             '{{title}}' => str_replace(['-','_'],' ', ucfirst($column)),
-            '{{value}}' =>  $value ? $this->getValue($column) : ''
+            '{{value}}' =>  $value ? $this->getValue($column, $model) : ''
         ]);
 
         return str_replace(
-            array_keys($replace), array_values($replace), $this->getStub("views/form-field")
+            array_keys($replace), array_values($replace), $this->getStub("views/form-field-$model")
         );
     }
 
     /**
      * @param $column
+     * @param $model
      *
      * @return mixed
      */
-    protected function getValue($column)
+    protected function getValue($column, $model)
     {
+        if($model == "check") {
+            $column .= ",\n\t\t\t".'"elements" => "checked=".${{modelNameLowerCase}}->'.$column;
+        }
         $replace = $this->buildReplacements();
 
         return str_replace(
--- resources/views/includes/check.blade.php
+++ resources/views/includes/check.blade.php
@@ -6,14 +6,14 @@
         $nameErrors = strstr($name,'[',true) !== false ? strstr($name,'[',true) : $name;
     @endphp
 
-    <div class="custom-control custom-checkbox my-1 mr-sm-2">
+    <div class="form-check">
         <input type="{{ $type ?? 'radio' }}"
-            class="custom-control-input {{ $class ?? "" }} {{ isset($nameErrors) && $errors->has($nameErrors) ? 'is-invalid' :'' }}"
+            class="form-check-input {{ $class ?? "" }} {{ isset($nameErrors) && $errors->has($nameErrors) ? 'is-invalid' :'' }}"
             id="{{ $id }}"
             value="{{ $value ?? true }}"
             {{ $name ? 'name=' . $name : '' }}
             {{ $elements ?? '' }}>
-        <label class="custom-control-label" for="{{ $id }}">{!! $label !!}</label>
+        <label class="form-check-label" for="{{ $id }}">{!! $label !!}</label>
 
         @isset($nameErrors)
         <div class="invalid-feedback">
