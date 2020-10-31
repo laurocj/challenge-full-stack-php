071723fa12f4ec2d8395a55ab17a7258bee9f8bd
laurocj <laurocj@gmail.com>
2020-10-31 00:10:19 -0300

    correct the order of the buttons

--- Modules/Education/Resources/views/student/create.blade.php
+++ Modules/Education/Resources/views/student/create.blade.php
@@ -6,8 +6,8 @@
     <div class="card-header d-flex justify-content-between">
         <span class="h2 my-auto">{{__('New record')}} - {{__('Student')}}</span>
         <div class="col-md-auto">
-            <button type="submit" class="btn btn-success">{{ __("Save") }}</button>
             <a class="btn btn-danger" href="{{ route('students.index') }}"> {{ __('Back') }}</a>
+            <button type="submit" class="btn btn-success">{{ __("Save") }}</button>
         </div>
     </div>
 
--- Modules/Education/Resources/views/student/edit.blade.php
+++ Modules/Education/Resources/views/student/edit.blade.php
@@ -7,8 +7,8 @@
     <div class="card-header d-flex justify-content-between">
         <span class="h2 my-auto">{{__('Edit record')}} - {{__('Student')}}</span>
         <div class="col-md-auto">
-            <button type="submit" class="btn btn-success">{{ __("Save") }}</button>
             <a class="btn btn-danger" href="{{ route('students.index') }}"> {{ __('Back') }}</a>
+            <button type="submit" class="btn btn-success">{{ __("Save") }}</button>
         </div>
     </div>
     <div class="card-body">
